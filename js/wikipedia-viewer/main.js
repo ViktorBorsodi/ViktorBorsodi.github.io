e2514a52c1ca075b4cdf31648b977bdbb224b1a5
Viktor Borsodi <34397487+ViktorBorsodi@users.noreply.github.com>
2018-03-27 18:36:24 +0200

    Add files via upload

--- js/wikipedia-viewer/main.js
+++ js/wikipedia-viewer/main.js
@@ -1,12 +1,13 @@
 var search = document.querySelector('#search');
 
-$("#search").on("change", function(e){
-	$("a").css("margin-top","10px");
-	$(".linkClass").remove();
-	$.ajax( {
-		url: "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cinfo&titles=Main%20Page&generator=search&exsentences=1&exintro=1&explaintext=1&exsectionformat=plain&inprop=url&gsrsearch=" + search.value + "&gsrinfo=totalhits%7Csuggestion%7Crewrittenquery",
-		dataType: 'jsonp',
-		success: function(data) {
+$("#search").on("keypress", function(e){
+	if(e.which == 13){
+		$("a").css("margin-top","10px");
+		$(".linkClass").remove();
+		$.ajax( {
+			url: "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cinfo&titles=Main%20Page&generator=search&exsentences=1&exintro=1&explaintext=1&exsectionformat=plain&inprop=url&gsrsearch=" + search.value + "&gsrinfo=totalhits%7Csuggestion%7Crewrittenquery",
+			dataType: 'jsonp',
+			success: function(data) {
 			// do something with data
 			var pages = data.query.pages;
 			for (var i in pages) {
@@ -20,6 +21,7 @@ $("#search").on("change", function(e){
 				link.setAttribute('href',pages[i].fullurl);
 				link.setAttribute('target','_blank');
 			}
-		}
-	} );
+			}
+		} );
+	}
 });
