c5631cd2b6d17b2c935de83b8456e1209ebe8f71
Viktor Borsodi <34397487+ViktorBorsodi@users.noreply.github.com>
2019-08-11 12:34:23 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+fetch('https://aqueous-dawn-66794.herokuapp.com/');
+fetch('https://serene-brook-41685.herokuapp.com/');
+fetch('https://ancient-savannah-23513.herokuapp.com/');
+fetch('https://yelpcamp-viktorborsodi.herokuapp.com/');
