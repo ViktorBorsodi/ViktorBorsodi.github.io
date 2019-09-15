de10f03b21d75ace83fd56ebc7d91bb2f0818c85
Viktor Borsodi <34397487+ViktorBorsodi@users.noreply.github.com>
2019-09-15 19:45:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 fetch('https://aqueous-dawn-66794.herokuapp.com/',{mode: 'no-cors'});
 fetch('https://serene-brook-41685.herokuapp.com/',{mode: 'no-cors'});
+fetch('https://warbler-server-vb.herokuapp.com/',{mode: 'no-cors'});
+fetch('https://warbler-client-vb.herokuapp.com/',{mode: 'no-cors'});
 fetch('https://ancient-savannah-23513.herokuapp.com/',{mode: 'no-cors'});
 fetch('https://yelpcamp-viktorborsodi.herokuapp.com/',{mode: 'no-cors'});
